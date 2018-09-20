a6e4da2686d467d86a433a32119dc2f862e72ffc
Anton Krylov <anthony.kryloff@gmail.com>
2018-09-20 23:42:32 +0300

    base project structure

---
+++ src/main/java/com/github/aekrylov/bootshare/BootshareApplication.java
@@ -0,0 +1,12 @@
+package com.github.aekrylov.bootshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootshareApplication.class, args);
+	}
+}

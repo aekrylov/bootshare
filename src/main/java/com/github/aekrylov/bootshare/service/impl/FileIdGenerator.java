4e907c1166f8480a1973fffff0b9876771057816
Anton Krylov <anthony.kryloff@gmail.com>
2018-09-24 00:11:50 +0300

    fix file id calculation

--- src/main/java/com/github/aekrylov/bootshare/service/impl/FileIdGenerator.java
+++ src/main/java/com/github/aekrylov/bootshare/service/impl/FileIdGenerator.java
@@ -7,12 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoField;
+
 /**
  * By Anton Krylov (anthony.kryloff@gmail.com)
  * Date: 9/23/18 6:13 PM
  */
 @Component
 public class FileIdGenerator {
+
+    private static final Instant START_DATE = Instant.ofEpochSecond(1535760000); //Sep 1st 2018
 
     private final Hashids hashids;
     private final FileInfoRepository fileInfoRepository;
@@ -26,7 +32,8 @@ public class FileIdGenerator {
     public String generate(User owner, MultipartFile file) {
         return hashids.encode(
                 owner.getId(),
-                fileInfoRepository.count()
+                Duration.between(START_DATE, Instant.now()).getSeconds(),
+                Instant.now().get(ChronoField.MILLI_OF_SECOND)
         ); //todo better generation algorithm
     }
 }
